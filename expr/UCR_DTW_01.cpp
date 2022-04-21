20c86e00391f98cd63275bdb3d8c18940cd173ae
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-04-21 03:30:05 +0000

    add C++ read log

--- expr/UCR_DTW_01.cpp
+++ expr/UCR_DTW_01.cpp
@@ -2,9 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 #include <iostream>
 
-#define min(x, y) ((x) < (y) ? (x) : (y))
-#define max(x, y) ((x) > (y) ? (x) : (y))
-#define dist(x, y) ((x - y) * (x - y))
+using namespace std;
+int main(int argc, char *argv[])
+{
+    FILE *fp; /// data file pointer
+    FILE *qp; /// query file pointer
 
+    fp = fopen(argv[1], "r");
+
+    char op[5];
+    char data[100];
+    int st, len;
+
+    while (fscanf(fp, "%s %d %d", op, &st, &len) != EOF)
+    {
+        if (strcmp(op, "DEL") == 0)
+        {
+            printf("%s %d %d\n", op, st, len);
+        }
+        else
+        {
+            fscanf(fp, "%s", data);
+            printf("%s %d %d %s\n", op, st, len, data);
+        }
+    }
+}
