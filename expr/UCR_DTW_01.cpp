29a8ea4f9f33478fc844b00897ef3a9a57393f14
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-04-21 07:03:34 +0000

    log+data -> repaired data

--- expr/UCR_DTW_01.cpp
+++ expr/UCR_DTW_01.cpp
@@ -4,29 +4,88 @@
 #include <time.h>
 #include <string.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 int main(int argc, char *argv[])
 {
-    FILE *fp; /// data file pointer
-    FILE *qp; /// query file pointer
+    // log file pointer
+    FILE *lp;
+    // data file pointer
+    FILE *fp;
 
     fp = fopen(argv[1], "r");
+    lp = fopen(argv[2], "r");
 
+    // 读取的log文件的一个操作
     char op[5];
+    // 读取的log文件一个操作后面的data
     char data[100];
+    // log文件一个操作后面的起始位置和操作长度
     int st, len;
 
-    while (fscanf(fp, "%s %d %d", op, &st, &len) != EOF)
+    // 最终还原出来的修复后的数据
+    vector<int> v;
+    // 每次读取的一个int数据
+    int d;
+    // data file的读取第pos个数
+    int pos = 0;
+    // log文件data的分隔符
+    const char *split = ",";
+    // log文件data通过split后的每一个char*类型的浮点数
+    char *sd;
+
+    while (fscanf(lp, "%s %d %d", op, &st, &len) != EOF)
     {
-        if (strcmp(op, "DEL") == 0)
+        while (pos < st)
         {
-            printf("%s %d %d\n", op, st, len);
+            fscanf(fp, "%d", &d);
+            v.push_back(d);
+            pos++;
         }
-        else
+        if (strcmp(op, "INS") == 0)
         {
-            fscanf(fp, "%s", data);
-            printf("%s %d %d %s\n", op, st, len, data);
+            fscanf(lp, "%s", data);
+            sd = strtok(data, split);
+            while (sd != NULL)
+            {
+                v.push_back(strtod(sd, NULL));
+                sd = strtok(NULL, split);
+            }
+        }
+        else if (strcmp(op, "REP") == 0)
+        {
+            fscanf(lp, "%s", data);
+            sd = strtok(data, split);
+            while (sd != NULL)
+            {
+                v.push_back(strtod(sd, NULL));
+                sd = strtok(NULL, split);
+            }
+            int end = pos + len;
+            while (pos < end)
+            {
+                fscanf(fp, "%d", &d);
+                pos++;
+            }
+        }
+        else if (strcmp(op, "DEL") == 0)
+        {
+            int end = pos + len;
+            while (pos < end)
+            {
+                fscanf(fp, "%d", &d);
+                pos++;
+            }
         }
     }
+    while (fscanf(fp, "%d", &d) != EOF)
+    {
+        v.push_back(d);
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+    return 0;
 }
---
+++ expr/test.cpp
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "stdio.h"
+
+int main ()
+{
+    char str[] ="a,b,c,d*e";
+    const char * split = ",";
+    char * p;
+    p = strtok (str,split);
+    while(p!=NULL) {
+        printf ("%s\n",p);
+        p = strtok(NULL,split);
+    } 
+
+    return 0;
+}
